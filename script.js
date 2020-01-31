452feae2fd8d9433e6c1fa677fd4277ab523aa24
Wade Butler <wadebutler2@gmail.com>
2020-01-30 21:37:55 -0500

    added cavas js made sketch pad functional

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const canvas = document.querySelector("#sketchpad");
 const context = canvas.getContext("2d");
+let painting = false; 
 
 window.addEventListener("load", () => {
     responsive();
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
 });
 
 window.addEventListener("resize", () => {
     responsive();
 })
+
+const startPosition = () => {
+    painting = true;
+    draw(event);
+}
+
+const finishedPosition = () => {
+    painting = false;
+    context.beginPath();
+}
+
+const draw = (event) => {
+    if(!painting) return;
+    context.lineWidth = 10;
+    context.lineCap = "round";
+
+    context.lineTo(event.clientX, event.clientY);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(event.clientX, event.clientY);
+}
 
 const responsive = () => {
     canvas.height = window.innerHeight;
