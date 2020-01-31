6a985d94941479529f3ad73d8a9af7a71cb9ef73
Wade Butler <wadebutler2@gmail.com>
2020-01-31 15:06:26 -0500

    adding touch draw?

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ window.addEventListener("load", () => {
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
     canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("touchstart", startPosition);
+    canvas.addEventListener("touchend", finishedPosition);
+    canvas.addEventListener("touchmove", draw);
 });
 
 window.addEventListener("resize", () => {
